ab8c72871b1f8722b692106cc074f996eda8ff3d
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-12-15 15:12:41 +0500

    chore: meta name for index root

--- app/root.tsx
+++ app/root.tsx
@@ -18,7 +18,7 @@ export const links: LinksFunction = () => {
 
 export const meta: MetaFunction = () => ({
   charset: "utf-8",
-  title: "Remix Notes",
+  title: "Kisan Diary",
   viewport: "width=device-width,initial-scale=1",
 });
 
