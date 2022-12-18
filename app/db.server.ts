3de806a00f0bcf98ecd2c0360b1ccf952914de8b
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-12-18 21:43:04 +0500

    chore: ignore all ts dist files and also fix envs

--- app/db.server.ts
+++ app/db.server.ts
@@ -1,6 +1,5 @@
 import { PrismaClient } from "@prisma/client"
 import invariant from "tiny-invariant";
-const env = process.env;
 let prisma: PrismaClient;
 
 declare global {
@@ -11,7 +10,7 @@ declare global {
 // the server with every change, but we want to make sure we don't
 // create a new connection to the DB with every change either.
 // in production we'll have a single connection to the DB.
-if (!env.isDev) {
+if (!(process.env.NODE_ENV === 'development')) {
   prisma = getClient();
 } else {
   if (!global.__db__) {
@@ -21,7 +20,7 @@ if (!env.isDev) {
 }
 
 function getClient() {
-  const { DATABASE_URL } = env;
+  const { DATABASE_URL } = process.env;
   invariant(typeof DATABASE_URL === "string", "DATABASE_URL env var not set");
 
   const databaseUrl = new URL(DATABASE_URL);
