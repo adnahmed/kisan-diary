21ce34e04017b46aae126aeed4281ec46515acb5
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-12-21 16:07:12 +0500

    feat: home page styling

--- app/routes/app/farmer.tsx
+++ app/routes/app/farmer.tsx
@@ -1,3 +1,11 @@
+import type { LinksFunction } from "@remix-run/server-runtime";
+import styles from "~/styles/routes/farmer.css";
+export const links: LinksFunction = () => {
+  return [
+    { rel: "stylesheet", href: styles },
+    { rel: "preload", href: "/assets/kisan_day.jpeg", as: "image" },
+  ];
+};
 export default function Farmer() {
-  return <div>Home</div>;
+  return <div className="home">Home</div>;
 }
