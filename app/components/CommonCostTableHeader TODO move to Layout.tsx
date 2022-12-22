3c3fc6b6b76588802c68385ede2896f34946cae3
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-12-22 16:56:12 +0500

    fix: donot ignore tailwindcss styles

--- app/components/CommonCostTableHeader TODO move to Layout.tsx
+++ app/components/CommonCostTableHeader TODO move to Layout.tsx
@@ -1,18 +1,30 @@
-import React, {FC} from 'react';
-import CellRow from './CellRow';
-import './CommonCostTableHeader.scss';
+import type { FC } from "react";
+import React from "react";
+import CellRow from "./cellRow";
+import "./CommonCostTableHeader.scss";
 
-export interface CommonCostTableHeaderProps {
-}
+export interface CommonCostTableHeaderProps {}
 
 const CommonCostTableHeader: FC<CommonCostTableHeaderProps> = (props) => (
-    <>
-        <CellRow style={{fontWeight: 'bold'}}
-                 values={['Operation/Activity', 'Best Practices', 'Conventional Practices']}/>
-        <CellRow values={['']}/>
-        <CellRow style={{fontWeight: 'bold'}} values={['', <CellRow values={['Quantity', 'Total Cost PKR']}/>,
-            <CellRow values={['Quantity', 'Total Cost PKR']}/>]}/>
-    </>
+  <>
+    <CellRow
+      style={{ fontWeight: "bold" }}
+      values={[
+        "Operation/Activity",
+        "Best Practices",
+        "Conventional Practices",
+      ]}
+    />
+    <CellRow values={[""]} />
+    <CellRow
+      style={{ fontWeight: "bold" }}
+      values={[
+        "",
+        <CellRow values={["Quantity", "Total Cost PKR"]} />,
+        <CellRow values={["Quantity", "Total Cost PKR"]} />,
+      ]}
+    />
+  </>
 );
 
 export default CommonCostTableHeader;
