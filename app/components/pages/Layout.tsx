bd9711866fea452c4fbcbcf8b5a438db6878e8ec
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-12-29 21:39:14 +0500

    feat: Layout component

---
+++ app/components/pages/Layout.tsx
@@ -0,0 +1,34 @@
+import type { User } from "@prisma/client";
+import { Role } from "@prisma/client";
+import type { ReactNode } from "react";
+import ExpertNavBar from "./ExpertNavBar";
+import FarmerNavBar from "./FarmerNavBar";
+import Footer from "./Footer";
+import Header from "./Header";
+
+interface LayoutProps {
+  user?: User;
+  children: ReactNode;
+}
+
+function useNavBar(user?: User) {
+  if (!user) return <></>;
+  switch (user.role) {
+    case Role.farmer:
+      return <FarmerNavBar />;
+    case Role.admin:
+      return <ExpertNavBar />;
+  }
+}
+
+export default function Layout({ user, children }: LayoutProps) {
+  const NavBar = useNavBar(user);
+  return (
+    <div className="layout">
+      <Header isAuthenticated={!!user} />
+      {NavBar}
+      {children}
+      <Footer />;
+    </div>
+  );
+}
