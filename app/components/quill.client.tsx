98870e628bae22dbdfd3a457c30c720ca3a7dd88
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2022-12-29 14:55:42 +0500

    fix: quill image upload

--- app/components/quill.client.tsx
+++ app/components/quill.client.tsx
@@ -4,35 +4,10 @@ import MagicUrl from "quill-magic-url";
 import type { ForwardedRef } from "react";
 import React, { useState } from "react";
 import ReactQuill, { Quill } from "react-quill";
-import saveToServer from "~/helpers/saveToServer.client";
 
 Quill.register("modules/imageUploader", ImageUploader);
 Quill.register("modules/magicUrl", MagicUrl);
 Quill.register("modules/better-table", BetterTable);
-
-const defaultModules = {
-  toolbar: [
-    [{ header: "1" }, { header: "2" }, { font: [] }],
-    [{ size: [] }],
-    ["bold", "italic", "underline", "strike", "blockquote"],
-    [
-      { list: "ordered" },
-      { list: "bullet" },
-      { indent: "-1" },
-      { indent: "+1" },
-    ],
-    ["link", "image", "video"],
-    ["clean"],
-  ],
-  clipboard: {
-    // toggle to add extra line breaks when pasting HTML:
-    matchVisual: false,
-  },
-  magicUrl: true,
-  imageUploader: {
-    upload: (file: File) => saveToServer(file),
-  },
-};
 
 const formats = [
   "header",
@@ -63,6 +38,43 @@ interface EditorProps {
 const Editor = React.forwardRef(
   (props: EditorProps, ref: ForwardedRef<ReactQuill>) => {
     const [editorHtml, setEditorHtml] = useState("");
+    const defaultModules = {
+      toolbar: [
+        [{ header: "1" }, { header: "2" }, { font: [] }],
+        [{ size: [] }],
+        ["bold", "italic", "underline", "strike", "blockquote"],
+        [
+          { list: "ordered" },
+          { list: "bullet" },
+          { indent: "-1" },
+          { indent: "+1" },
+        ],
+        ["link", "image", "video"],
+        ["clean"],
+      ],
+      clipboard: {
+        // toggle to add extra line breaks when pasting HTML:
+        matchVisual: false,
+      },
+      magicUrl: true,
+      imageUploader: {
+        upload: async (file: File) => {
+          const body = new FormData();
+          body.append("file", file);
+          try {
+            const result = await fetch("/api/save_file", {
+              method: "POST",
+              body: body,
+            });
+            const { uploadedUrl } = await result.json();
+            return uploadedUrl;
+          } catch (error) {
+            if (typeof error === "string") console.error("Error:", error);
+          }
+        },
+      },
+    };
+
     return (
       <ReactQuill
         value={editorHtml}
