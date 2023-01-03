c01f024348746caebbdaaab8d26119b984c81e29
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-01-03 13:26:24 +0500

    feat: useLastMatch helper

---
+++ app/helpers/useLastMatch.tsx
@@ -0,0 +1,7 @@
+import { useMatches } from "@remix-run/react";
+
+export default function useLastMatch() {
+  const matches = useMatches();
+  const lastMatch = matches.slice(-1)[0];
+  return lastMatch.pathname.split("/")[-1];
+}
