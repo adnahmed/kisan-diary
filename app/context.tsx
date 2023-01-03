a69f383b409b7d42295444ada5873474a2d6ab56
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-01-04 02:08:36 +0500

    feat: socket context

--- app/context.tsx
+++ app/context.tsx
@@ -1,6 +1,11 @@
 // context.tsx
 import { createContext } from "react";
+import type { Socket } from "socket.io-client";
 
+/* Socket-Io Context */
+export const SocketIOContext = createContext<Socket | undefined>(undefined);
+
+/* Emotion Style Context */
 export interface ServerStyleContextData {
   key: string;
   ids: Array<string>;
