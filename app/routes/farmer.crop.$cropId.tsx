1df6986a65cdaf6d15768cef685eb5cc4a4c2920
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-01-14 13:10:38 +0500

    feat: sidebar navbar

--- app/routes/farmer.crop.$cropId.tsx
+++ app/routes/farmer.crop.$cropId.tsx
@@ -1,9 +1,44 @@
-import { Link, useCatch } from "@remix-run/react";
 import type { LinksFunction, LoaderArgs } from "@remix-run/node";
+import {
+  Link,
+  Outlet,
+  useCatch,
+  useLocation,
+  useParams,
+} from "@remix-run/react";
+import { useEffect } from "react";
+import { redirect } from "remix-typedjson";
+import type { RouteParams } from "routes-gen";
+import { route } from "routes-gen";
+import slugify from "slugify";
 import styles from "~/styles/routes/farmer.crop.css";
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: styles }];
 };
+const menu = [
+  "Land Preparation",
+  "Sowing",
+  "Inputs",
+  "Harvesting",
+  "Post Harvesting and Storage",
+  "Marketing",
+  "All Costs",
+  "Economic Analysis",
+];
+const InputsMenu = [
+  "Seed",
+  "Irrigation",
+  "Labor Management",
+  "Integrated Pest Management",
+  "Integrated Disease Management",
+  "Integrated Weed Management",
+  "Nutrient Management",
+  "Gross Inputs Cost",
+];
+export const handle = {
+  menu__item: "/farmer/crops",
+};
+
 export function CatchBoundary() {
   const caught = useCatch();
 
@@ -17,6 +52,7 @@ export function CatchBoundary() {
     </div>
   );
 }
+
 export function ErrorBoundary({ error }: { error: Error }) {
   return (
     <div>
@@ -27,25 +63,102 @@ export function ErrorBoundary({ error }: { error: Error }) {
     </div>
   );
 }
-export async function loader({ request }: LoaderArgs) {
-  return {};
+
+export async function loader({ request, params }: LoaderArgs) {
+  const { cropId } = params as RouteParams["/farmer/crop/:cropId"];
+  // TODO: convert to callback style functions
+  const url = new URL(request.url);
+  const pathname = url.pathname;
+  const last = pathname.split("/").at(-1);
+  const firstMenu = menu.at(0);
+  if (!firstMenu) return {};
+  if (!last) return {};
+  const redirectUrl =
+    route(`/farmer/crop/:cropId`, { cropId: cropId }) + `/${toUrl(menu[0])}`;
+  if (!menu.map((menu__item) => toUrl(menu__item)).includes(last))
+    return redirect(redirectUrl);
+  else return {};
 }
+
 export default function CropCard() {
   return (
-    <div className="crop-card">
-      <div style={{ color: "green" }} className="heading">
+    <div className="crop__card card__wrapper">
+      <div style={{ color: "green" }} className="crop__card card__heading">
         Farm Functions
       </div>
-      <div className="crop-card options">
-        <Link to="land-preparation">Land Preparation</Link>
-        <Link to="sowing">Sowing</Link>
-        <Link to="inputs">Inputs</Link>
-        <Link to="harvesting">Harvesting</Link>
-        <Link to="post-harvesting-and-storage">Post Harvest And Storage</Link>
-        <Link to="marketing">Marketing</Link>
-        <Link to="all-costs">All Costs</Link>
-        <Link to="economic-analysis">Economic Analysis</Link>
+      <div className="crop__card card__menu">
+        <FunctionsSidebar />
+      </div>
+      <div className="crop__card card__content">
+        <Outlet />
       </div>
     </div>
+  );
+}
+
+const toUrl = (menu__item: string) =>
+  slugify(menu__item, { lower: true, trim: true }).replace(/-/g, "_");
+
+const useHighlight = () => {
+  const location = useLocation();
+  return (menu__item: string) =>
+    location.pathname.split("/").at(-1) === toUrl(menu__item);
+};
+function FunctionsSidebar() {
+  const location = useLocation();
+  const { cropId } = useParams<RouteParams["/farmer/crop/:cropId"]>();
+  const InputRoute =
+    route("/farmer/crop/:cropId", { cropId: cropId || "" }) + "/inputs";
+  const highlight = useHighlight();
+  useEffect(() => console.log(`input_route: ${InputRoute}`));
+  return (
+    <div className="card__sidebar">
+      {menu.map((menu__item: string) =>
+        menu__item === "Inputs" ? (
+          <>
+            <Link
+              to={toUrl(menu__item)}
+              key={"input"}
+              className={`crop__card card__sidebar sidebar__key ${
+                highlight(menu__item) ? "key--selected" : ""
+              }`}
+            >
+              Inputs
+            </Link>
+            {location.pathname.startsWith(InputRoute) && (
+              <div className="crop__card card__sidebar sidebar__key--2">
+                {InputsMenu.map((menu__item: string) => (
+                  <MenuLink
+                    key={menu__item}
+                    menu__item={menu__item}
+                    sub="inputs/"
+                  />
+                ))}
+              </div>
+            )}
+          </>
+        ) : (
+          <MenuLink key={menu__item} menu__item={menu__item} />
+        )
+      )}
+    </div>
+  );
+}
+interface Props {
+  menu__item: string;
+  sub?: string;
+}
+function MenuLink({ menu__item, sub }: Props) {
+  const highlight = useHighlight();
+  return (
+    <Link
+      className={`crop__card card__sidebar sidebar__key ${
+        highlight(menu__item) ? "key--selected" : ""
+      }`}
+      key={menu__item}
+      to={(sub ?? "") + toUrl(menu__item)}
+    >
+      {menu__item}
+    </Link>
   );
 }
