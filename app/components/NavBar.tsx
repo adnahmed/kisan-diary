216544137c7d7fac84305f97633c672253738fe7
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-01-14 13:10:16 +0500

    fix: navbar invert

--- app/components/NavBar.tsx
+++ app/components/NavBar.tsx
@@ -1,12 +1,14 @@
 import { Box, Link } from "@chakra-ui/react";
 import { useLocation } from "@remix-run/react";
 import slugify from "slugify";
+import useLastMatch from "../helpers/useLastMatch";
 import CABIButton from "./cabi-button";
 interface NavBarProps {
   dashboardPath: string;
   menu: string[];
 }
 export default function NavBar({ dashboardPath, menu }: NavBarProps) {
+  const lastMatch = useLastMatch();
   const location = useLocation();
   const url = (menu_item: string) =>
     dashboardPath + slugify(menu_item.replace("/", "-"), { lower: true });
@@ -15,9 +17,10 @@ export default function NavBar({ dashboardPath, menu }: NavBarProps) {
       {menu.map((menu_item) => (
         <Link variant="cabi" key={menu_item} className="" href={url(menu_item)}>
           <CABIButton
-            invertVariant={new RegExp(`${url(menu_item)}.*`).test(
-              location.pathname
-            )}
+            invertVariant={
+              new RegExp(`${url(menu_item)}.*`).test(location.pathname) ||
+              lastMatch.handle?.menu__item === url(menu_item)
+            }
           >
             {menu_item}
           </CABIButton>
