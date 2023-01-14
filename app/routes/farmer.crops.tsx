00371af67832e259d045b724e98e903717e31321
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-01-14 13:12:41 +0500

    fix: crop page

--- app/routes/farmer.crops.tsx
+++ app/routes/farmer.crops.tsx
@@ -1,11 +1,4 @@
-import {
-  Accordion,
-  AccordionButton,
-  AccordionIcon,
-  AccordionItem,
-  AccordionPanel,
-  Box,
-} from "@chakra-ui/react";
+import { Box } from "@chakra-ui/react";
 import type { LinksFunction, LoaderArgs } from "@remix-run/node";
 import { Link } from "@remix-run/react";
 import { typedjson, useTypedLoaderData } from "remix-typedjson";
@@ -15,10 +8,12 @@ import { prisma } from "~/db.server";
 import fetchFarm from "~/models/farm.server";
 import { getUser } from "~/session.server";
 import styles from "~/styles/routes/farmer.crops.css";
+import CABIButton from "../components/cabi-button";
 
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: styles }];
 };
+
 export async function loader({ request }: LoaderArgs) {
   const user = await getUser(request);
   if (!user) throw new Error("Unauthorized!");
@@ -36,40 +31,44 @@ export async function loader({ request }: LoaderArgs) {
     }),
   });
 }
+
 export default function Crops() {
-  const data = useTypedLoaderData<typeof loader>();
-  const deleteCrop = (id: string) => {
-    // setCropsList(cropList.filter((c) => c.id !== id));
-  };
   return (
     <div className="crops__dashboard">
       <NewCropModal />
-      <Accordion allowToggle>
-        <AccordionItem>
-          <AccordionButton _expanded={{ bg: "cabi", color: "wheat" }}>
-            <Box
-              display={"flex"}
-              as="span"
-              flex="1"
-              textAlign="left"
-              justifyContent={"space-between"}
-            >
-              <span>All Crops</span>
-              <AccordionIcon />
-            </Box>
-          </AccordionButton>
-          <AccordionPanel pb={4}>
-            {data.crops.map((c) => (
-              <div key={c.id}>
-                <Link to={route(`/farmer/crop/${crop.id}`)}>
-                  <span>{c.name}</span>
-                </Link>
-                <button onClick={() => deleteCrop(c.id)}>Delete</button>
-              </div>
-            ))}
-          </AccordionPanel>
-        </AccordionItem>
-      </Accordion>
+      <AllCropsList />
     </div>
+  );
+}
+
+function AllCropsList() {
+  const data = useTypedLoaderData<typeof loader>();
+  return (
+    <>
+      <Box
+        display={"flex"}
+        as="span"
+        bg={"cabi"}
+        color={`wheat`}
+        flex="1"
+        textAlign="left"
+        justifyContent={"space-between"}
+      >
+        <span className="allCrops__heading">All Crops</span>
+      </Box>
+      {data.crops.map((c) => (
+        <div key={c.id} className="allCrops__crops crops__crop">
+          <Link
+            className="allCrops__crops crops__data--name"
+            to={route(`/farmer/crop/:cropId`, { cropId: c.id })}
+          >
+            <span>{c.name}</span>
+          </Link>
+          <CABIButton className="allCrops__crops crop__action--delete">
+            Delete
+          </CABIButton>
+        </div>
+      ))}
+    </>
   );
 }
