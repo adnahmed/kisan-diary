99da42ebe2f95227d565601dac601b0d236d4704
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-01-16 20:13:03 +0500

    feat: operation row

---
+++ app/components/spreadsheet/OperationRow.tsx
@@ -0,0 +1,33 @@
+import type { Row, TextCell } from "@silevis/reactgrid";
+import { boldText, nonEditable } from "./CellTypes";
+export const OperationHeader: TextCell = {
+  type: "text",
+  text: "Operation/Activity",
+  nonEditable: true,
+  renderer: (text) => <b>{text}</b>,
+};
+export const OperationRow: Row = {
+  rowId: "op_header",
+  height: 50,
+  cells: [
+    OperationHeader,
+    boldText(
+      nonEditable({
+        type: "text",
+        text: "Unit Cost",
+      }) as TextCell
+    ),
+    boldText(
+      nonEditable({
+        type: "text",
+        text: "Quantity",
+      }) as TextCell
+    ),
+    boldText(
+      nonEditable({
+        type: "text",
+        text: "Total Cost",
+      }) as TextCell
+    ),
+  ],
+};
