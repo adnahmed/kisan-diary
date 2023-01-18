511cae5181a003143595de43eb1ca22f869db55d
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-01-18 19:55:32 +0500

    fix: land preparation seed

--- prisma/activities/land_preparation.ts
+++ prisma/activities/land_preparation.ts
@@ -4,23 +4,15 @@ const LandPreparationSeed: ActivityCreateInput[] = [
     {
         type: ActivityType.landPreparation,
         name: "Land preparation (disk plough, cultivator, suhaga, chesil, rotavator)",
-        isOperation: false,
-        quanity: 0,
+        quantity: 0,
         unitCost: 0,
     },
     {
         type: ActivityType.landPreparation,
         name: "Planter",
-        isOperation: false,
-        quanity: 0,
+        quantity: 0,
         unitCost: 0,
     },
-    {
-        type: ActivityType.landPreparation,
-        name: "Gross Land Preparation Cost",
-        isOperation: true,
-        quanity: 0,
-        unitCost: 0,
-    },
+
 ]
 export default LandPreparationSeed;
