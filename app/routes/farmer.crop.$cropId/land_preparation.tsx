7f0380d0be979ac5c495b78160b6e56e84f3cc71
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-01-18 19:58:55 +0500

    feat: store activity action

--- app/routes/farmer.crop.$cropId/land_preparation.tsx
+++ app/routes/farmer.crop.$cropId/land_preparation.tsx
@@ -1,22 +1,51 @@
 import { ActivityType } from "@prisma/client";
-import type { LoaderArgs } from "@remix-run/node";
-import { useCatch } from "@remix-run/react";
+import type { ActionArgs, LoaderArgs } from "@remix-run/node";
+import { useCatch, useFetcher, useTransition } from "@remix-run/react";
 import type { LinksFunction } from "@remix-run/server-runtime";
-import type { CellChange, Row } from "@silevis/reactgrid";
+import type { CellChange, NumberCell, Row } from "@silevis/reactgrid";
 import { ReactGrid } from "@silevis/reactgrid";
-import { typedjson, useTypedLoaderData } from "remix-typedjson";
+import { useEffect } from "react";
+import {
+  typedjson,
+  useTypedActionData,
+  useTypedLoaderData,
+} from "remix-typedjson";
 import { HeaderRow } from "~/components/spreadsheet/CellTypes";
 import { OperationRow } from "~/components/spreadsheet/OperationRow";
 import { Columns } from "~/components/spreadsheet/TitleColumn";
 import { prisma } from "~/db.server";
 import ActivitiyWithCostRows from "~/helpers/activity";
 import styles from "~/styles/routes/farmer.crop.land_preparation.css";
+import { boldText, textCell } from "../../components/spreadsheet/CellTypes";
+
 export const handle = {
   menu__item: "/farmer/crops",
 };
+
 export const links: LinksFunction = () => {
   return [{ rel: "stylesheet", href: styles }];
 };
+
+export async function action({ request }: ActionArgs) {
+  const formData = await request.formData();
+  const id = formData.get("id")?.toString();
+  const key = formData.get("key")?.toString();
+  const value = formData.get("value")?.toString();
+  const num = Number(value);
+  if (!id || !key || !value) return null;
+  const updateQuery = {
+    where: {
+      id: id,
+    },
+    data: {
+      [key]: num,
+    },
+  };
+  await prisma.activity.update({
+    ...updateQuery,
+  });
+  return typedjson(updateQuery);
+}
 export async function loader({ request }: LoaderArgs) {
   return typedjson({
     activities: await prisma.activity.findMany({
@@ -30,18 +59,32 @@ export interface LandPreparationProps {}
 
 export default function LandPreparation(props: LandPreparationProps) {
   const { activities } = useTypedLoaderData<typeof loader>();
+  const data = useTypedActionData<typeof action>();
   const Rows: Row[] = [
     OperationRow,
     HeaderRow("Land Preparation"),
     ...ActivitiyWithCostRows(activities),
+    {
+      rowId: "gross",
+      cells: [boldText(textCell("Gross Land Preparation Cost"))],
+    },
   ];
-
+  const fetcher = useFetcher();
+  useEffect(() => console.log(`Action: ${JSON.stringify(fetcher.data)}`));
+  const transition = useTransition();
   const handleChanges = (changes: CellChange[]) => {
-    changes.forEach((change: CellChange) => {});
+    changes.forEach((change: CellChange<NumberCell>) => {
+      const formData = new FormData();
+      formData.set("id", change.rowId.toString());
+      formData.set("key", change.columnId.toString());
+      formData.set("value", change.newCell.value.toString());
+      fetcher.submit(formData, { method: "post" });
+    });
   };
 
   return (
     <div className="crop__content content__landPreparation">
+      {transition.state === "submitting" ? <p>Saving...</p> : null}
       <ReactGrid
         rows={Rows}
         columns={Columns}
