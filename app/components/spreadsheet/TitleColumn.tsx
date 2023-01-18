d42e3b3429d653d2a38b5a8e2df13cd903b58148
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-01-18 19:56:50 +0500

    feat: column ids

--- app/components/spreadsheet/TitleColumn.tsx
+++ app/components/spreadsheet/TitleColumn.tsx
@@ -3,10 +3,10 @@ import { COL_WIDTH } from "./CellTypes";
 
 export const Columns: Column[] = [
   {
-    columnId: "titlesColumn",
+    columnId: "nameColumn",
     width: 500,
   },
-  { columnId: 1, width: COL_WIDTH },
-  { columnId: 2, width: COL_WIDTH },
-  { columnId: 3, width: COL_WIDTH },
+  { columnId: "quantity", width: COL_WIDTH },
+  { columnId: "unitCost", width: COL_WIDTH },
+  { columnId: "totalCost", width: COL_WIDTH },
 ];
