13095075e0c2390f82e1a5b5e0d66cb6dd73347e
Adnan Ahmed Khan <mailtoadnan.ahmed@gmail.com>
2023-01-20 19:50:35 +0500

    feat: useToggle hook

---
+++ app/components/hooks/useToggle.tsx
@@ -0,0 +1,25 @@
+// Hook
+
+import { useCallback, useState } from "react";
+
+// Parameter is the boolean, with default "false" value
+const useToggle = (initialState = false) => {
+  // Initialize the state
+  const [state, setState] = useState(initialState);
+
+  // Define and memorize toggler function in case we pass down the component,
+  // This function change the boolean value to it's opposite value
+  const toggle = useCallback(() => setState((state) => !state), []);
+
+  return [state, toggle];
+};
+
+export default useToggle;
+
+// Example
+// Call the hook which returns, current value and the toggler function
+//    const [isTextChanged, setIsTextChanged] = useToggle();
+
+//    return (
+//        <button onClick={setIsTextChanged}>{isTextChanged ? 'Toggled' : 'Click to Toggle'}</button>
+//    );
